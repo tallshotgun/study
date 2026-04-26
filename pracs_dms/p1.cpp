36f32e8d4883c47b1dcfc45b11eda491b02ae8e8
tallshotgun <saumyasehrawat07@gmail.com>
2026-04-26 16:31:55 +0530

    dms pras

--- pracs_dms/p1.cpp
+++ pracs_dms/p1.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int global = 0;
+
+int main()
+{
+    int x = 10;
+    ++global;
+    {
+        x = 0;
+        cout << x << endl;
+    }
+    cout << x << endl;
+    return 0;
+
+}
